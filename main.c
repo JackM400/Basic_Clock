75fb907e06e761dc3b501074658dcfa850d01b58
JackM400 <jack.millar400@gmail.com>
2019-08-13 18:05:47 +0100

    choices added errors removed , year print out incorrect [119]

--- main.c
+++ main.c
@@ -3,12 +3,31 @@
 #include <time.h>
 
 typedef struct tm tm;
+
 int main() {
+    int choice;
     time_t currentTime;
-    time(currentTime);
+    time(&currentTime);
 
     tm *localTime = localtime(&currentTime);
-    int x = localTime ->tm_sec;
-    printf("%i",&x);
+
+    printf("Would you like the day , month , year or all [0 , 1 , 2 , 3]\n");
+    scanf("%d", &choice);
+
+    if (choice == 0) {
+        printf("%i",localTime->tm_mday);
+    }
+    if (choice == 1) {
+        printf("%i",localTime->tm_mon);
+    }
+    if (choice == 2) {
+        printf("%i",localTime->tm_year);
+    }
+    if (choice == 3) {
+        printf("%i,%i,%i",localTime->tm_mday,localTime->tm_mon,localTime->tm_year);
+        //TODO year is 119?
+    }
+
+
     return 0;
 }
