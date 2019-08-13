cda38401c17720185c9fe7ea57869f3a0dde128e
JackM400 <jack.millar400@gmail.com>
2019-08-13 17:41:35 +0100

    variables created , initial time assigned , loops created

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <conio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int i;
+    int hour = 0;
+    int minute = 0;
+    int second = 0;
+
+    printf("Input current time [HH:MM:SS]\n");
+    scanf("%d%d%d", &hour, &minute, &second);
+    //3 loops , for hour , minute , second , update clock on each successful completion of inner loop
+
+    for (int hour; hour < 24; hour++) {
+        for (int minute; minute < 60; minute++) {
+            for (int seconds; seconds < 60; seconds++) {
+
+            }
+        }
+    }
     return 0;
 }
